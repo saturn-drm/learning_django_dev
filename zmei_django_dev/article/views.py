35e9c11b3549270b3aa54b71573186eb1706c6c8
Zihan Mei <meizihanyx@gmail.com>
2020-11-10 01:22:57 +0800

    2020-11-10

--- zmei_django_dev/article/views.py
+++ zmei_django_dev/article/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ArticlePost
 
 # Create your views here.
+def article_list(request):
+    articles = ArticlePost.objects.all()
+
+    context = { 'articles': articles }
+
+    return render(request, 'article/list.html', context)
